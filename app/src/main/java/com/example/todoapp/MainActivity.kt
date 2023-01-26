b80e8d0e411edd97b5cb0861c37876d628aa5d06
Andrey <kezin636@gmail.com>
2023-01-27 00:01:10 +0300

    Changes:
    - Fixed hardcode with colors
    - Image placeholder and Id both have more sense in name
    - Fixed the WHOLE layout
    - Now primary and secondary activity colors are yellow and orange

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+
         setContentView(R.layout.activity_main)
     }
 }
